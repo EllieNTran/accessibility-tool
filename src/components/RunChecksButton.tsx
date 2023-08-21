25dc82194f44286a8a79b0f92816f617c7284fe9
etran3 <Ellie.Tran@kpmg.co.uk>
2023-08-21 17:20:28 +0100

    add previous code

---
+++ src/components/RunChecksButton.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+const RunChecksButton = (): JSX.Element => (
+  <Button id="run-checks-button">Run Npm Command</Button>
+);
+
+export default RunChecksButton;
